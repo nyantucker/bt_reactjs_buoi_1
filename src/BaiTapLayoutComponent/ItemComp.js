aa8cc51978e53bf0c5ef99fbc4086c5bfd5ef5e0
nyantucker <ltnhan1911@gmail.com>
2023-08-28 16:25:40 +0700

    2nd push

--- src/BaiTapLayoutComponent/ItemComp.js
+++ src/BaiTapLayoutComponent/ItemComp.js
@@ -8,7 +8,7 @@ export default class ItemComp extends Component {
         <div key={index} className="col-lg-6 col-xl-4 mb-5" style={{fontSize:"15px"}}>
           <div className="card bg-light border-0 h-100">
           <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                <div style={{height: '4rem', width: '4rem', fontSize: '2rem', justifyContent: 'center', position: 'absolute', top: 0, left: '50%', transform: 'translateX(-50%)'}} className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                <div style={{height: '4rem', width: '4rem', fontSize: '2rem', justifyContent: 'center', position: 'absolute', top: 0, left: '50%', transform: 'translateX(-50%)',lineHeight:`4rem`}} className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
                     <i className={item} />
           </div>
           <h5 className="fs-4 fw-bold">{title}</h5>
